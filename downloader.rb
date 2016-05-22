9db5859c0b216127809a1c318453564ea8f3953e
Duhee Lee <doo2@classting.com>
2016-05-22 21:37:10 +0900

    add

---
+++ downloader.rb
@@ -0,0 +1,4 @@
+1.step(3991, 10) do |i|
+  puts i
+  `curl -o iamdoo2/result_#{i}.html "https://search.naver.com/search.naver?ie=utf8&where=news&query=이두희&sm=tab_pge&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&mynews=0&start=#{i}&refresh_start=0"`
+end
