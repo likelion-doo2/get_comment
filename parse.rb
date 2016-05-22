a1d1b1da6ea1cc540d95b4a0b7f9d43bbdede480
Duhee Lee <doo2@classting.com>
2016-05-22 21:40:23 +0900

    수정했습니다.

--- parse.rb
+++ parse.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 
 1.step(3991, 10) do |i|
-  doc = Nokogiri::HTML(open("iamdoo2/result_#{i}.html"))
+  doc = Nokogiri::HTML(open("result_#{i}.html"))
   doc.css("._sp_each_url").each do |x|
     puts x.attr("href") if x.attr("href").include?("naver.com")
   end
